9572ed27d0ae30d2473f0944a07f869cd21cea4f
Bar <Barmagnezi22@gmail.com>
2015-06-19 17:20:59 +0300

    added blog controller and defualt view

---
+++ MvcApplication1/MvcApplication1/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        private PostDBContext db = new PostDBContext();
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
